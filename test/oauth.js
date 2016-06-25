b0405a71481e475c90c35919c9365ab208818c3c
User <user@dispostable.com>
2016-06-25 14:23:37 +0800

    new es6 rewrite

---
+++ test/oauth.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import PCS from '../lib/pcs';
+import config from './config.json';
+
+const UID = 1107596645;
+const APP_ID = 1569193;
+
+const pcs = new PCS(config);
+test('getLoggedInUser', async t => {
+  const result = await pcs.api.getLoggedInUser();
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.deepEqual(Object.keys(result), ['uid', 'uname', 'portrait']);
+});
+
+test('getInfo', async t => {
+  const result = await pcs.api.getInfo();
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(typeof result.userid, 'undefined');
+});
+
+test('isAppUser', async t => {
+  let result = await pcs.api.isAppUser();
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.is(result.result, '1', 'result == 1');
+  result = await pcs.api.isAppUser({ uid: UID, appid: APP_ID });
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.is(result.result, '1', 'result == 1');
+});
---
+++ test/pcs.js
@@ -0,0 +1,82 @@
+import test from 'ava';
+import PCS from '../lib/pcs';
+import tmp from 'tmp';
+import fs from 'fs';
+import config from './config.json';
+
+const pcs = new PCS(config);
+test('quota', async t => {
+  const result = await pcs.api.quota();
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.deepEqual(Object.keys(result), ['quota', 'used', 'request_id']);
+});
+
+test.serial('mkdir', async t => {
+  const result = await pcs.api.mkdir('testD');
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+});
+
+test.serial('fileUpload', async t => {
+  const result = await pcs.api.fileUpload('../package.json', 'testD/u.js', 'overwrite');
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(result.path, undefined);
+});
+
+test.serial('copy', async t => {
+  let result = await pcs.api.copy({ from: 'testD', to: 'testD2' });
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(result.extra, undefined);
+  result = await pcs.api.copy([{ from: 'testD/u.js', to: 'testD/d.js' }, { from: 'testD/u.js', to: 'testD/dup.js' }]);
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(result.extra, undefined);
+});
+
+test.serial('move', async t => {
+  let result = await pcs.api.move({ from: 'testD2', to: 'testD1' });
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(result.extra, undefined);
+  result = await pcs.api.move([{ from: 'testD1/u.js', to: 'testD1/1.json' }, { from: 'testD/dup.js', to: 'testD1/2.json' }]);
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.not(result.extra, undefined);
+});
+
+test.serial('fileDownload', async t => {
+  const tmpFile = tmp.fileSync();
+  await pcs.api.fileDownload('testD/d.js', tmpFile.name);
+  t.is(fs.readFileSync(tmpFile.name, 'utf8'), fs.readFileSync('../package.json', 'utf8'));
+  tmpFile.removeCallback();
+});
+
+test.serial('meta', async t => {
+  let result = await pcs.api.meta('testD');
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.true(result.list instanceof Array);
+  result = await pcs.api.meta(['testD', 'testD/d.js']);
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.true(result.list instanceof Array && result.list.length === 2);
+});
+
+test.serial('list', async t => {
+  const result = await pcs.api.list('testD', 'name', 'asc', '0-10');
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  t.true(result.list instanceof Array);
+});
+
+test.serial('del', async t => {
+  let result = await pcs.api.del(['testD/u.js', 'testD/d.js', 'testD']);
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+  result = await pcs.api.del('testD1');
+  t.is(typeof result, 'object', 'result is object');
+  t.is(typeof result.error_code, 'undefined', 'there\'s no errors');
+});
